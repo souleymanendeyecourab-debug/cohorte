b170b9f3e6991f86bb98e5f39d9532805ec4baac
Souleymane Ndeye Coura Ba <Souleymanendeyecourab@gmail.com>
2026-08-24 22:39:01 +0000

    feat(moderation): integrer OpenRouter pour moderer publications et reponses

--- config/services.php
+++ config/services.php
@@ -13,6 +13,10 @@ return [
     | a conventional file to locate the various service credentials.
     |
     */
+    'openrouter' => [
+    'api_key' => env('OPENROUTER_API_KEY'),
+    'model' => env('OPENROUTER_MODEL', 'meta-llama/llama-3.1-8b-instruct:free'),
+],
 
     'postmark' => [
         'key' => env('POSTMARK_API_KEY'),
